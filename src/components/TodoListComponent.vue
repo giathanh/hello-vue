cf981fc3b2cda0589fd2b6754636a8f98d0b3d16
Chau Lai Gia Thanh <chaulaigiathanh@gmail.com>
2020-12-13 21:19:17 +0700

    init source

---
+++ src/components/TodoListComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li>Todo A</li>
+      <li>Todo B</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class TodoListComponent extends Vue {}
+</script>
---
+++ src/views/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="todo-list">
+    <TodoListComponent />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import TodoListComponent from "@/components/TodoListComponent.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    TodoListComponent
+  }
+})
+export default class TodoList extends Vue {}
+</script>
